a62c7263a02f921b255d0422a11d22a6c864ed06
Kirill <kirillpromonte@gmail.com>
2025-07-03 17:29:02 +0200

    Добавлена агрегация по колонке через аргумент --aggregate

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from utils import read_csv_file, print_table
 import argparse
-from proccesor import filter_data
+from proccesor import filter_data, aggregate
 
 
 def read_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--where")
-
+    parser.add_argument("--aggregate")
     return parser.parse_args()
 
 def split_where_arg(where_str):
-    for op in [">=","<=","<",">","="]:
-        if op in where_str:
-            header, value = where_str.split(op)
-            return header.strip(), op, value.strip()
+    for operator in [">=","<=","<",">","="]:
+        if operator in where_str:
+            column, op_value = where_str.split(operator)
+            return column.strip(), operator, op_value.strip()
 
     raise ValueError("Неверный формат для --where. Используй =, <, >, <=, >=")
+
+def split_aggregate_arg(aggregate_str):
+    if "=" not in aggregate_str:
+        raise ValueError("Неверный формат для --aggregate. Используй = ")
+
+    column, op = aggregate_str.split("=")
+    return column.strip(), op.strip()
 
 if __name__ == '__main__':
     args = read_args()
@@ -26,3 +33,10 @@ if __name__ == '__main__':
         data = filter_data(data,header,op,value)
 
     print_table(data)
+
+    if args.aggregate:
+        column, agg_op = split_aggregate_arg(args.aggregate)
+        result = aggregate(data, column, agg_op)
+        print(f"{agg_op} по колонке {column}: {result}")
+    else:
+        print_table(data)
--- proccesor.py
+++ proccesor.py
@@ -22,3 +22,23 @@ def filter_data(data, header, operator, value):
             res.append(row)
 
     return res
+
+def aggregate(data, column, op):
+    values = []
+    for row in data:
+        if column not in row:
+            raise ValueError(f"Колонка '{column}' не найдена в данных")
+        try:
+            number = float(row[column])
+            values.append(number)
+        except ValueError:
+            raise ValueError("Нет данных для агрегации")
+
+    if op == "avg":
+        return sum(values) / len(values)
+    elif op == "min":
+        return min(values)
+    elif op == "max":
+        return max(values)
+    else:
+        raise ValueError(f"Операция '{op}' не поддерживается")
