6fcde043afa9162018830c730d88a78470e84383
Kirill <kirillpromonte@gmail.com>
2025-07-03 16:36:49 +0200

    Добавлена фильтрация по аргументу --where

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from utils import read_csv_file, print_table
 import argparse
+from proccesor import filter_data
 
-
-if __name__ == '__main__':
-    data = read_csv_file("sample.csv")
-    print_table(data)
 
 def read_args():
     parser = argparse.ArgumentParser()
@@ -19,3 +16,13 @@ def split_where_arg(where_str):
             return header.strip(), op, value.strip()
 
     raise ValueError("Неверный формат для --where. Используй =, <, >, <=, >=")
+
+if __name__ == '__main__':
+    args = read_args()
+    data = read_csv_file("sample.csv")
+
+    if args.where:
+        header, op, value = split_where_arg(args.where)
+        data = filter_data(data,header,op,value)
+
+    print_table(data)
